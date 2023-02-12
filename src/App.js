9c6162049ac17cfc94fb03ea3e63712bd985c833
Tharindu Gimras <tharindugimras@gmail.com>
2023-02-12 22:35:34 +0530

    Add axios and make it to do GET requests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [deviceName, setDeviceName] = useState();
+  const [deviceColor, setDeviceColor] = useState();
+  const [deviceCapacity, setDeviceCapacity] = useState();
+
+  const url = 'https://api.restful-api.dev/objects';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(url);
+        console.log(result);
+        console.log('working');
+        const neededData = result.data[2];
+
+        setDeviceName(neededData.name);
+        setDeviceColor(neededData.data.color);
+        setDeviceCapacity(neededData.data['capacity GB']);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="device">{deviceName}</div>
+      <div className="device-color">{deviceColor}</div>
+      <div className="device-capacity">{deviceCapacity}</div>
     </div>
   );
 }
+
+// axios
+// rest api - get, post, put, delete
+// update, patch
+
+// ['a','b','c','d']
 
 export default App;
